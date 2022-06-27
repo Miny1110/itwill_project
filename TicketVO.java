7e52ccfa335cf2b713ccae7522dc68dbd14ec0f6
Miny1110 <wjdalswjd453@naver.com>
2022-06-27 20:58:33 +0900

    Create TicketVO.java

---
+++ TicketVO.java
@@ -0,0 +1,38 @@
+package project1;
+
+public class TicketVO {
+
+//	private int choice;
+	private int theater;
+	private int moive;
+	private int date;
+	
+	/*
+	public int getChoice() {
+		return choice;
+	}
+	public void setChoice(int choice) {
+		this.choice = choice;
+	}
+	*/
+	public int getTheater() {
+		return theater;
+	}
+	public void setTheater(int theater) {
+		this.theater = theater;
+	}
+	public int getMoive() {
+		return moive;
+	}
+	public void setMoive(int moive) {
+		this.moive = moive;
+	}
+	public int getDate() {
+		return date;
+	}
+	public void setDate(int date) {
+		this.date = date;
+	}
+	
+	
+}
