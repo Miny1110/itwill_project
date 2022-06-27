e8f4a4b99cb0821d3873e24ed2abfccee3b7557a
Miny1110 <wjdalswjd453@naver.com>
2022-06-27 20:58:27 +0900

    Create TheaterVO.java

---
+++ TheaterVO.java
@@ -0,0 +1,31 @@
+package project1;
+
+public class TheaterVO {
+
+	private int theater;
+	private int date;
+	private int movie;
+	
+	
+	public int getTheater() {
+		return theater;
+	}
+	public void setTheater(int theater) {
+		this.theater = theater;
+	}
+	public int getDate() {
+		return date;
+	}
+	public void setDate(int date) {
+		this.date = date;
+	}
+	public int getMovie() {
+		return movie;
+	}
+	public void setMovie(int movie) {
+		this.movie = movie;
+	}
+	
+	
+	
+}
