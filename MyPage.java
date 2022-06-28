bd1c283b01744891a4b2bb3a135a4d6cc926b67b
Miny1110 <wjdalswjd453@naver.com>
2022-06-28 18:08:58 +0900

    Create MyPage.java

---
+++ MyPage.java
@@ -0,0 +1,13 @@
+package project1;
+
+public interface MyPage {
+	
+	public void mypageMain();
+	public void checkBooking();
+	public void cancleBooking();
+	public void modifyBooking();
+	public void searchInfo();
+	public void logout();
+	
+
+}
