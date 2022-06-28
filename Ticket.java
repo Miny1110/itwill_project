43f9754b7d0178ee60cf7ceee5389ee4a3c90deb
Miny1110 <wjdalswjd453@naver.com>
2022-06-28 18:09:07 +0900

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -2,7 +2,7 @@ package project1;
 
 public interface Ticket {
 	
-//	public void choice(); //예매방법선택
+	public void choice(); //예매방법선택
 	public void theater(); //상영관예매
 	public void movie(); //영화예매
 	public void date(); //날짜예매
