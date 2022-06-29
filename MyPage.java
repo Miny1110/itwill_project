9bc1e504fc1bbccc4b1191f7697bd6681b782efe
Miny1110 <wjdalswjd453@naver.com>
2022-06-29 14:49:49 +0900

    Update MyPage.java

--- MyPage.java
+++ MyPage.java
@@ -1,13 +1,14 @@
 package project1;
 
+import java.util.List;
+
 public interface MyPage {
 	
-	public void mypageMain();
+	public void mypageMain(MyPageVO vo);
 	public void checkBooking();
-	public void cancleBooking();
+	public void cancleBooking(Ticket1VO voT);
 	public void modifyBooking();
 	public void searchInfo();
 	public void logout();
-	
 
 }
