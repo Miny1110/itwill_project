7f1efef35895751d2bdde66a9551d246376306f7
Miny1110 <wjdalswjd453@naver.com>
2022-06-29 14:49:50 +0900

    Update MyPageImpl.java

--- MyPageImpl.java
+++ MyPageImpl.java
@@ -5,30 +5,42 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.TreeMap;
 
-public class MyPageImpl extends StartPageImpl implements MyPage{
+public class MyPageImpl implements MyPage{
 	
 	Scanner sc=new Scanner(System.in);
-	//Map<String, MyPageVO> hMap=new TreeMap<>();
-	//Map<String, Ticket1Impl> hMap2=new TreeMap<>();
-	Ticket1Impl tkImpl = new Ticket1Impl();
 	
+	MyPageVO vo = null;
+	Ticket1VO voT = null;
+	private List<Ticket1VO>listsT1 = null;
+
+	
+	public MyPageImpl(List<Ticket1VO> listsT1) {
+		this.listsT1=listsT1;
+	}
+	
+	public MyPageImpl(Ticket1VO voT) {
+		this.voT=voT;
+	}
+	
+	public MyPageImpl() {
+		
+	}
 	
 	@Override
-	public void mypageMain() {
+	public void mypageMain(MyPageVO vo) {
 		
-		new MyPageImpl();
 		
+		this.vo = vo;
 		while(true) {
 			System.out.println("1.예매확인 2.예매변경 3.예매취소 4.내 정보 5.로그아웃");
 			int ch=sc.nextInt();
-			//System.out.println(hMap);
+			
 
 			switch(ch) {
 			case 1: checkBooking(); break;
-			case 2: cancleBooking(); break;
-			case 3: modifyBooking(); break;
+			case 2: modifyBooking(); break;
+			case 3: cancleBooking(voT); break;
 			case 4: searchInfo(); break;
 			case 5: logout(); break;
 		
@@ -36,46 +48,61 @@ public class MyPageImpl extends StartPageImpl implements MyPage{
 			System.exit(0);
 			}
 		}
-
+		
+		
+		
+		
+		
 	}
 
 	@Override
 	public void checkBooking() {
 		
-		
-		Iterator<Ticket1VO> it = tkImpl.listsT1.iterator();
-		while(it.hasNext()) {
-			Ticket1VO vo = it.next();
-			vo.print();
-		}
-		
-		/*
-		Iterator<String> it = hMap2.keySet().iterator();
-		while(it.hasNext()) {
-			String key = it.next(); //key
-			
-			if(key.equals(id)) {
-				tki = hMap2.get(id); //value를 꺼내온다.
-				
-			}
-		}
-		*/
-		
 	}
 
 	@Override
-	public void cancleBooking() {
+	public void cancleBooking(Ticket1VO voT) {
+		
+		
+		Scanner sc= new Scanner(System.in);
+		//this.listsTheater;
+		
+		
+		Iterator<Ticket1VO> it = listsT1.iterator();
+		voT=it.next();
+		
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
+		
+		System.out.println("취소하시겠습니까? [yes/no]");
+		String a=sc.next();
+		
+		if(a=="yes") {
+			listsT1.remove(voT);
+			System.out.println("취소되었습니다.");
+		}else System.out.println("종료합니다.");
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 	@Override
 	public void modifyBooking() {
+		
 	}
 
 	@Override
 	public void searchInfo() {
 		
-		MyPageVO vo = hMap.get(checkID);
-			
 		System.out.println("이름 : "+vo.getName());
 		System.out.println("회원등급 : "+vo.getLevel());
 		System.out.println("포인트 : "+vo.getPoint());
@@ -87,7 +114,5 @@ public class MyPageImpl extends StartPageImpl implements MyPage{
 		System.out.println("로그아웃을 완료하였습니다.");
 		System.exit(0);
 	}
-
-
 
 }
