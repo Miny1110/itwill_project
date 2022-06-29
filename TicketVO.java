6f54c6288dc79f33dd7a629869543edb5f7a967e
Miny1110 <wjdalswjd453@naver.com>
2022-06-29 19:49:23 +0900

    Update TicketVO.java

--- TicketVO.java
+++ TicketVO.java
@@ -1,42 +1,19 @@
 package project1;
 
 public class TicketVO {
-
-//	private int choice;
-	private String theater;
-	private String moive;
-	private String date;
-	private int inwon;
-	private int seatRow;
-	private int seatNum;
-	private int tot;
-	private int pay;
 	
-	/*
-	public int getChoice() {
-		return choice;
+	private String seat[];
+	private int inwon;
+	private int pay;
+	private int tot;
+
+	
+	
+	public String[] getSeat() {
+		return seat;
 	}
-	public void setChoice(int choice) {
-		this.choice = choice;
-	}
-	*/
-	public String getTheater() {
-		return theater;
-	}
-	public void setTheater(String theater) {
-		this.theater = theater;
-	}
-	public String getMoive() {
-		return moive;
-	}
-	public void setMoive(String moive) {
-		this.moive = moive;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
+	public void setSeat(String[] seat) {
+		this.seat = seat;
 	}
 	public int getInwon() {
 		return inwon;
@@ -44,28 +21,14 @@ public class TicketVO {
 	public void setInwon(int inwon) {
 		this.inwon = inwon;
 	}
-	public int getSeatRow() {
-		return seatRow;
-	}
-	public void setSeatRow(int seatRow) {
-		this.seatRow = seatRow;
-	}
-	public int getSeatNum() {
-		return seatNum;
-	}
-	public void setSeatNum(int seatNum) {
-		this.seatNum = seatNum;
-	}
-	public int getTot() {
-		return inwon*10000;
-	}
 	public int getPay() {
 		return pay;
 	}
 	public void setPay(int pay) {
 		this.pay = pay;
 	}
+	public int getTot() {
+		return inwon * 10000;
+	}
 
-	
-	
 }
